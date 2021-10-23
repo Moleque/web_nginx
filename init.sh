be6a9ea0c2e2e946b11e029e22fa4a039ee75753
d.salman <d.salman@city-mobil.ru>
2021-10-23 19:55:27 +0300

    django

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart 
 # sudo /etc/init.d/gunicorn restart
 
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
 sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.ask.wsgi:application
