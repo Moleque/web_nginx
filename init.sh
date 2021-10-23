23e0fc6d870aca371f2e45bb990be2f653b520f7
d.salman <d.salman@city-mobil.ru>
2021-10-24 00:25:55 +0300

    django

--- init.sh
+++ init.sh
@@ -3,11 +3,7 @@ sudo pip3 install --upgrade django==2.0.7
 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 
-sudo /etc/init.d/nginx restart 
-# sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/gunicorn-django.py
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.py
-
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf hello:app
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf.py hello:app
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf.py ask.wsgi:application
