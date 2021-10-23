c29f4dc79d076cbb49e4114a610d29c41b723bf1
d.salman <d.salman@city-mobil.ru>
2021-10-23 18:59:46 +0300

    django

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='test'),
-    path('login/', views.test, name='test'),
-    path('signup/', views.test, name='test'),
-    path('question/<int:id>', views.test, name='test'),
-    path('ask/', views.test, name='test'),
-    path('popular/', views.test, name='test'),
-    path('new/', views.test, name='test'),
+    path('', views.test, name='index'),
+    path('login/', views.test, name='login'),
+    path('signup/', views.test, name='signup'),
+    path('question/<int:id>', views.test, name='question'),
+    path('ask/', views.test, name='ask'),
+    path('popular/', views.test, name='popular'),
+    path('new/', views.test, name='new'),
 ]
--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
+sudo pip3 install --upgrade django==2.0.7
+
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+
 sudo /etc/init.d/nginx restart 
-# sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 hello:app
+sudo /etc/init.d/gunicorn restart
+# sudo gunicorn -b 0.0.0.0:8000 hello:app
+
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
