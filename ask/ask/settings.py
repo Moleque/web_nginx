b53e84970fd3d404a4e9b40e609cb66b2d39835e
d.salman <d.salman@city-mobil.ru>
2021-10-24 00:21:37 +0300

    django

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'ask',
+    # 'qa',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- init.sh
+++ init.sh
@@ -6,7 +6,8 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart 
 # sudo /etc/init.d/gunicorn restart
 
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf.py /etc/gunicorn.d/gunicorn-django.py
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/gunicorn-django.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.py
 
-# sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app &
-sudo gunicorn -c /etc/gunicorn.d/gunicorn-django.py ask.ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf hello:app
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
